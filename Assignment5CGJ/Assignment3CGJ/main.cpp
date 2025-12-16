ea5bec8cbd4b92c3a8772a59c553abb88de946eb
Guilherme Calçadas <guilherme.c.calcadas@gmail.com>
2025-12-16 17:07:01 +0000

    Added the 3 main objects

--- Assignment5CGJ/Assignment3CGJ/main.cpp
+++ Assignment5CGJ/Assignment3CGJ/main.cpp
@@ -40,7 +40,6 @@ private:
         bool isOrtho;
     };
     CameraInfo cam1;
-    CameraInfo cam2;
     CameraInfo* activeCam = nullptr;
     glm::vec3 target = glm::vec3(0.0f, 0.5f, 0.0f);
 
@@ -63,10 +62,14 @@ private:
     // Meshes
     std::vector<mgl::Mesh*> MeshesList;
 	mgl::Mesh* woodenSwordMesh = nullptr;
+    mgl::Mesh* candleMesh = nullptr;
+    mgl::Mesh* pedestalMesh = nullptr;
 
     // Scene Graph
     mgl::SceneNode* root = nullptr;
     mgl::SceneNode* woodenSwordNode = nullptr;
+    mgl::SceneNode* candleNode = nullptr;
+    mgl::SceneNode* pedestalNode = nullptr;
     std::vector<mgl::SceneNode*> nodes;
 
     // Animation parameters
@@ -83,7 +86,6 @@ private:
     void drawMesh(mgl::Mesh* m, glm::vec3 pos, float rotX, float rotY, float rotZ, float scal);
     void createSceneGraph();
     static void calculateProjection(CameraInfo& cam, int width, int height);
-    void updatePieceTransforms(float dt);
 };
 
 ///////////////////////////////////////////////////////////////////////// MESHES
@@ -113,6 +115,14 @@ void MyApp::createMeshes() {
     woodenSwordMesh = new mgl::Mesh();
     woodenSwordMesh->joinIdenticalVertices();
     woodenSwordMesh->create(mesh_dir + "wooden_sword.obj");
+
+    candleMesh = new mgl::Mesh();
+    candleMesh->joinIdenticalVertices();
+    candleMesh->create(mesh_dir + "candle.obj");
+
+    pedestalMesh = new mgl::Mesh();
+    pedestalMesh->joinIdenticalVertices();
+    pedestalMesh->create(mesh_dir + "pedestal.obj");
 }
 
 
@@ -139,97 +149,23 @@ void MyApp::createShaderPrograms() {
 }
 
 
-struct TransformationConfiguration {
-    glm::vec3 pos;
-    float rotX;
-    float rotY;
-    float rotZ;
-};
-
-const float SIDE = 0.89f; //length of a side of the pickagram in square form
-
-TransformationConfiguration sealConfig[7] = {
-    { glm::vec3(0.4232f, 0.8375f, 0.0f), 0.0f, 0.0f, 108.0f }, //triangle 1
-    { glm::vec3(0.7628f, 0.8108f, 0.0f), 0.0f, 0.0f, 153.0f }, //triangle 2
-    { glm::vec3(1.0905f, 0.8204f, 0.0f), 0.0f, 0.0f, 18.0f }, //triangle 4
-    { glm::vec3(-0.0688f, 0.9116f, 0.0f), 0.0f, 0.0f, 198.0f }, //triangle 6
-    { glm::vec3(0.0053f, 1.4036f, 0.0f), 0.0f, 0.0f, 18.0f }, // triangle 7
-    { glm::vec3(-0.1402, 0.7714f, 0.0f), 0.0f, 0.0f, 288.0f }, //paralellogram 3a
-    { glm::vec3(0.0741f, 1.1920f, 0.0f), 0.0f, 0.0f, 198.0f } //square 5
-};
-
-TransformationConfiguration squareConfig[7] = {
-    { glm::vec3(-SIDE / 2 - 0.5f, 0.5f, 0.0f), 90.0f, 0.0f, -90.0f }, //triangle 1
-    { glm::vec3(-0.5f, 0.5f, -SIDE / 2), 90.0f, 0.0f, 0.0f }, //triangle 2
-    { glm::vec3(-0.5f, 0.5f, SIDE / 4), 90.0f, 0.0f, 0.0f }, //triangle 4
-    { glm::vec3(SIDE / 2 - 0.5f, 0.5f, -SIDE / 4), 90.0f, 0.0f, 0.0f }, //triangle 6
-    { glm::vec3(SIDE / 4 - 0.5f, 0.5f, SIDE / 4), 90.0f, 0.0f, 0.0f }, // triangle 7
-    { glm::vec3(-SIDE / 8 - 0.5f, 0.5f, SIDE / 4 + SIDE / 8), 90.0f, 0.0f, 0.0f }, //paralellogram 3a
-    { glm::vec3(SIDE / 4 - 0.5f, 0.5f, 0.0f), 90.0f, 0.0f, 0.0f } //square 5
-};
-
 void MyApp::createSceneGraph() {
     root = new mgl::SceneNode(nullptr, nullptr);
-    TangramPiece* woodenSword = new TangramPiece(woodenSwordMesh, glm::vec4(0.6f, 0.4f, 0.2f, 1.0f));
+
+    TangramPiece* pedestal = new TangramPiece(pedestalMesh, glm::vec4(0.6f, 0.4f, 0.2f, 1.0f));
+    pedestalNode = new mgl::SceneNode(pedestal, Shaders);
+    pedestalNode->transform = glm::mat4(1.0f);
+    root->addChild(pedestalNode);
+
+    TangramPiece* woodenSword = new TangramPiece(woodenSwordMesh, glm::vec4(0.2f, 0.4f, 0.8f, 1.0f));
     woodenSwordNode = new mgl::SceneNode(woodenSword, Shaders);
     woodenSwordNode->transform = glm::mat4(1.0f);
-    root->addChild(woodenSwordNode);
-    /*
-    // Mesa
-    TangramPiece* tablePiece = new TangramPiece(MeshesList[0], glm::vec4(0.6f, 0.4f, 0.2f, 1.0f));
-    tableNode = new mgl::SceneNode(tablePiece, Shaders);
-    tableNode->transform = glm::mat4(1.0f);
-    root->addChild(tableNode);
+    pedestalNode->addChild(woodenSwordNode);
 
-    // Pe?a 0 - Triangle 1
-    TangramPiece* p0 = new TangramPiece(MeshesList[1], glm::vec4(0.9f, 0.1f, 0.1f, 1.0f));
-    auto n0 = new mgl::SceneNode(p0, Shaders);
-    n0->transform = getModel(squareConfig[0].pos, squareConfig[0].rotX, squareConfig[0].rotY, squareConfig[0].rotZ, 1.0f);
-    tableNode->addChild(n0);
-    nodes.push_back(n0);
-
-    // Pe?a 1 - Triangle 2
-    TangramPiece* p1 = new TangramPiece(MeshesList[2], glm::vec4(0.1f, 0.9f, 0.1f, 1.0f));
-    auto n1 = new mgl::SceneNode(p1, Shaders);
-    n1->transform = getModel(squareConfig[1].pos, squareConfig[1].rotX, squareConfig[1].rotY, squareConfig[1].rotZ, 1.0f);
-    tableNode->addChild(n1);
-    nodes.push_back(n1);
-
-    // Pe?a 2 - Triangle 4
-    TangramPiece* p2 = new TangramPiece(MeshesList[3], glm::vec4(0.1f, 0.1f, 0.9f, 1.0f));
-    auto n2 = new mgl::SceneNode(p2, Shaders);
-    n2->transform = getModel(squareConfig[2].pos, squareConfig[2].rotX, squareConfig[2].rotY, squareConfig[2].rotZ, 1.0f);
-    tableNode->addChild(n2);
-    nodes.push_back(n2);
-
-    // Pe?a 3 - Triangle 6
-    TangramPiece* p3 = new TangramPiece(MeshesList[4], glm::vec4(0.9f, 0.9f, 0.1f, 1.0f));
-    auto n3 = new mgl::SceneNode(p3, Shaders);
-    n3->transform = getModel(squareConfig[3].pos, squareConfig[3].rotX, squareConfig[3].rotY, squareConfig[3].rotZ, 1.0f);
-    tableNode->addChild(n3);
-    nodes.push_back(n3);
-
-    // Pe?a 4 - Triangle 7
-    TangramPiece* p4 = new TangramPiece(MeshesList[5], glm::vec4(0.9f, 0.1f, 0.9f, 1.0f));
-    auto n4 = new mgl::SceneNode(p4, Shaders);
-    n4->transform = getModel(squareConfig[4].pos, squareConfig[4].rotX, squareConfig[4].rotY, squareConfig[4].rotZ, 1.0f);
-    tableNode->addChild(n4);
-    nodes.push_back(n4);
-
-    // Pe?a 5 - Paralelogram
-    TangramPiece* p5 = new TangramPiece(MeshesList[6], glm::vec4(0.1f, 0.9f, 0.9f, 1.0f));
-    auto n5 = new mgl::SceneNode(p5, Shaders);
-    n5->transform = getModel(squareConfig[5].pos, squareConfig[5].rotX, squareConfig[5].rotY, squareConfig[5].rotZ, 1.0f);
-    tableNode->addChild(n5);
-    nodes.push_back(n5);
-
-    // Pe?a 7 - Quadrado
-    TangramPiece* p7 = new TangramPiece(MeshesList[7], glm::vec4(0.8f, 0.8f, 0.8f, 1.0f));
-    auto n7 = new mgl::SceneNode(p7, Shaders);
-    n7->transform = getModel(squareConfig[6].pos, squareConfig[6].rotX, squareConfig[6].rotY, squareConfig[6].rotZ, 1.0f);
-    tableNode->addChild(n7);
-    nodes.push_back(n7);
-    */
+    TangramPiece* candle = new TangramPiece(candleMesh, glm::vec4(0.1f, 0.5f, 0.2f, 1.0f));
+    candleNode = new mgl::SceneNode(candle, Shaders);
+    candleNode->transform = glm::mat4(1.0f);
+    root->addChild(candleNode);
 }
 
 ///////////////////////////////////////////////////////////////////////// CAMERA
@@ -247,18 +183,7 @@ void MyApp::createCamera() {
     cam1.viewMatrix = glm::lookAt(eye1, target, up1);
     cam1.isOrtho = false;
 
-    // --- SETUP CAM 2 ---
-    glm::vec3 eye2(-5.0f, 2.0f, -5.0f);
-    glm::vec3 up2(0.0f, 1.0f, 0.0f);
-
-    cam2.radius = glm::length(eye2);
-    cam2.rotation = glm::quatLookAt(glm::normalize(-eye2), up2);
-    cam2.viewMatrix = glm::lookAt(eye2, target, up2);
-    cam2.isOrtho = false;
-
     calculateProjection(cam1, 800, 600);
-    calculateProjection(cam2, 800, 600);
-
     activeCam = &cam1;
     Camera->setViewMatrix(activeCam->viewMatrix);
     Camera->setProjectionMatrix(activeCam->projectionMatrix);
@@ -313,23 +238,6 @@ void MyApp::calculateProjection(CameraInfo& cam, int width, int height) {
     }
 }
 
-void MyApp::updatePieceTransforms(float dt) {
-    t += animDirection * animSpeed * dt;
-    t = glm::clamp(t, 0.0f, 1.0f);
-
-    for (int i = 0; i < nodes.size(); i++) {
-
-        glm::vec3 pos = glm::mix(squareConfig[i].pos,
-            sealConfig[i].pos, t);
-
-        float rotX = glm::mix(squareConfig[i].rotX, sealConfig[i].rotX, t);
-        float rotY = glm::mix(squareConfig[i].rotY, sealConfig[i].rotY, t);
-        float rotZ = glm::mix(squareConfig[i].rotZ, sealConfig[i].rotZ, t);
-
-        nodes[i]->transform = getModel(pos, rotX, rotY, rotZ, 1.0f);
-    }
-}
-
 
 ////////////////////////////////////////////////////////////////////// CALLBACKS
 
@@ -343,40 +251,17 @@ void MyApp::initCallback(GLFWwindow* win) {
 void MyApp::windowSizeCallback(GLFWwindow* win, int width, int height) {
     glViewport(0, 0, width, height);
     calculateProjection(cam1, width, height);
-    calculateProjection(cam2, width, height);
     if (Camera && activeCam) {
         Camera->setProjectionMatrix(activeCam->projectionMatrix);
     }
 }
 
 void MyApp::displayCallback(GLFWwindow* win, double elapsed) {
-    updatePieceTransforms(elapsed);
     drawScene();
 }
 
 void MyApp::keyCallback(GLFWwindow* win, int key, int scancode, int action, int mods) {
-
-    if (key == GLFW_KEY_RIGHT) {
-        if (action == GLFW_PRESS) animDirection = +1;
-        if (action == GLFW_RELEASE) animDirection = 0;
-    }
-
-    if (key == GLFW_KEY_LEFT) {
-        if (action == GLFW_PRESS) animDirection = -1;
-        if (action == GLFW_RELEASE) animDirection = 0;
-    }
-    if (action != GLFW_PRESS) return;
     switch (key) {
-
-    case GLFW_KEY_C:
-        activeCam = (activeCam == &cam1) ? &cam2 : &cam1;
-
-        Camera->setViewMatrix(activeCam->viewMatrix);
-        Camera->setProjectionMatrix(activeCam->projectionMatrix);
-
-        std::cout << "Camera: " << (activeCam == &cam1 ? "1" : "2") << std::endl;
-        break;
-
     case GLFW_KEY_P:
         activeCam->isOrtho = !activeCam->isOrtho;
         int w, h;
